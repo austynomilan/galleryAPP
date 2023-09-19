37d7add41d1966fbdefa1775e4fd7b519a3ce488
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-09-19 22:54:31 +0100

    feat: drag and drop implemented

--- galleryAPP/src/Home/Home.jsx
+++ galleryAPP/src/Home/Home.jsx
@@ -2,11 +2,55 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Nav/nav';
 import 'react-loading-skeleton/dist/skeleton.css';
 import data from '../Data/data.json';
+import { closestCenter, DndContext } from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import CardSkeleton from '../components/ui_Kit/cardSkeleton';
 import './Home.scss';
 
+
+const SortableImage = ({ image }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: image.id });
+
+  const style = {
+    transition: transition ?? '',
+    transform: CSS.Transform.toString(transform),
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      style={style}
+      {...listeners}
+      className='image_holder'
+    >
+      <img src={image['image-url']} alt={image.description} />
+    </div>
+  );
+};
+
 export default function Home() {
   const [loading, setLoading] = useState(true);
+  const [images, setImages] = useState(data.GirlImages);
+
+  const onDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setImages((prevImages) => {
+      const oldIndex = prevImages.findIndex((image) => image.id === active.id);
+      const newIndex = prevImages.findIndex((image) => image.id === over.id);
+      return arrayMove(prevImages, oldIndex, newIndex);
+    });
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,15 +63,20 @@ export default function Home() {
       <div className='homeContainer'>
         <Navbar />
         <div className='image_card'>
-          {loading ? (
-            <CardSkeleton numCards={25} />
-          ) : (
-            data.GirlImages.map((image) => (
-              <div className='image_holder' key={image.id}>
-                {<img src={image['image-url']} alt='' />}
-              </div>
-            ))
-          )}
+          <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+            <SortableContext
+              items={images}
+              strategy={verticalListSortingStrategy}
+            >
+              {loading ? (
+                <CardSkeleton numCards={25} />
+              ) : (
+                images.map((image) => (
+                  <SortableImage key={image.id} image={image} />
+                ))
+              )}
+            </SortableContext>
+          </DndContext>
         </div>
       </div>
     </>
