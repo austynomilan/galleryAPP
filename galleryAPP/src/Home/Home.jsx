1f83795ecede11b9e711167764023203f94dca8c
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-09-21 11:02:17 +0100

    notification updated

--- galleryAPP/src/Home/Home.jsx
+++ galleryAPP/src/Home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   const [login, setLogin] = useState(false);
 
   const toggleLoginState = () => {
-    setLogin(!login); 
+    setLogin(!login);
   };
 
   const handleCloseSignIn = () => {
@@ -22,14 +22,19 @@ export default function Home() {
 
   const handleImageDrag = () => {
     setShowLogin(true);
-  }; 
+  };
+
+  const handleTouchImageDrag = () => {
+    // Handle touch-based drag action here
+    setShowLogin(true);
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, []);
-  
+
   const handleSearchChange = (value) => {
     setSearch(value);
   };
@@ -37,20 +42,35 @@ export default function Home() {
   return (
     <>
       <div className='homeContainer'>
-        <Navbar onSearchChange={handleSearchChange} login={login} toggleLoginState={toggleLoginState} search={search}/>
+        <Navbar
+          onSearchChange={handleSearchChange}
+          login={login}
+          toggleLoginState={toggleLoginState}
+          search={search}
+        />
         <div className='image_card'>
-        {showLogin && <SignIn toggleLoginState={toggleLoginState}  handleCloseSignIn={handleCloseSignIn} />}
+          {showLogin && (
+            <SignIn
+              toggleLoginState={toggleLoginState}
+              handleCloseSignIn={handleCloseSignIn}
+            />
+          )}
           {loading ? (
             <CardSkeleton numCards={25} />
           ) : (
-            data.GirlImages
-            .filter((image) =>
+            data.GirlImages.filter((image) =>
               image.description.toLowerCase().includes(search.toLowerCase())
-            )
-            .map((image) => (
-              
+            ).map((image) => (
               <div className='image_holder' key={image.id}>
-                {<img src={image['image-url']} draggable="true" alt={image.description} onDragStart={handleImageDrag}/>}
+                {
+                  <img
+                    src={image['image-url']}
+                    draggable='true'
+                    alt={image.description}
+                    onDragStart={handleImageDrag}
+                    onTouchStart={handleTouchImageDrag}
+                  />
+                }
                 <span>{image.description}</span>
               </div>
             ))
